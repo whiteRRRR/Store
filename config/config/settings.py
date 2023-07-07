a9ef8be1343c674ee1a7e148e66120ab267ec92a
whiteRR <whiterr2134@gmail.com>
2023-07-07 13:52:38 +0600

    Renamed app directory

--- config/config/settings.py
+++ config/config/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Apps
-    'online_shop.apps.OnlineShopConfig',
+    'product.apps.Product',
 ]
 
 MIDDLEWARE = [
--- config/product/apps.py
+++ config/product/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OnlineShopConfig(AppConfig):
+class Product(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'online_shop'
+    name = 'product'
