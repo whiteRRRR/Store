2f170766342f74e3eecc44d6028cb779674b415f
whiteRR <whiterr2134@gmail.com>
2023-07-12 16:09:49 +0600

    Added class CommentBlog

--- config/apps/blog/models.py
+++ config/apps/blog/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-
+from apps.user.models import CustomUser
 
 class Blog(models.Model):
     """
     Создает класс для блога
     """
     title = models.CharField(max_length=150, verbose_name='Название блога')
-    description = models.TextField(blank=True, verbose_name='Информация о блоге')
+    description = models.TextField(verbose_name='Информация о блоге')
     image = models.ImageField(upload_to='blog_image/', verbose_name='Картинка для блога')
     date = models.DateField(auto_now=True, verbose_name='Дата создание')
     slug = models.SlugField(max_length=100, verbose_name='URL адрес блога')
-    category = models.OneToOneField('BlogCategory', on_delete=models.PROTECT, verbose_name='Категория')
-    tag = models.ManyToManyField('BlogTags', verbose_name='Тэги')
+    category = models.ForeignKey('BlogCategory', on_delete=models.PROTECT, verbose_name='Категория')
+    tags = models.ManyToManyField('BlogTags', verbose_name='Тэги')
 
     def __str__(self):
         return self.title
@@ -50,6 +50,15 @@ class BlogTags(models.Model):
         verbose_name = 'Тэг'
         verbose_name_plural = 'Тэги'
 
+
+class CommentBlog(models.Model):
+    """
+     Создает модель комментария для блога
+     """
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    content = models.TextField(verbose_name='Содержание комментария')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Автор комментария')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время создания')
 
 
 
