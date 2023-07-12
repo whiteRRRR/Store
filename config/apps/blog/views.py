b954478959e214b355ae0665edc1363415904f9a
whiteRR <whiterr2134@gmail.com>
2023-07-12 16:12:40 +0600

    Added paginator and search by categories and tags

--- config/apps/blog/views.py
+++ config/apps/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
+from django.db.models import Count
 from apps.blog.models import *
 
 
@@ -10,6 +11,7 @@ def index(request):
 
 class BlogListView(ListView):
     model = Blog
+    paginate_by = 1
     template_name = 'blog/blog.html'
     context_object_name = 'blogs'
 
@@ -20,7 +22,45 @@ class BlogListView(ListView):
         return context
 
 
+class ByCategoryView(ListView):
+    model = Blog
+    paginate_by = 1
+    template_name = 'blog/blog.html'
+    context_object_name = 'blogs'
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        category = BlogCategory.objects.get(slug=category_slug)
+        return Blog.objects.filter(category=category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = BlogCategory.objects.all()
+        context['tags'] = BlogTags.objects.all()
+        return context
+
+
+class ByTagView(ListView):
+    paginate_by = 1
+    model = Blog
+    template_name = 'blog/blog.html'
+    context_object_name = 'blogs'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug']
+        tag = BlogTags.objects.get(slug=tag_slug)
+        return Blog.objects.filter(tags=tag)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = BlogCategory.objects.all()
+        context['tags'] = BlogTags.objects.all()
+        return context
+
+
+
 class BlogSearchView(ListView):
+    paginate_by = 1
     model = Blog
     template_name = 'blog/blog.html'
     context_object_name = 'blogs'
@@ -47,6 +87,8 @@ class BlogDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tags'] = BlogTags.objects.all()
+        context['blogs'] = Blog.objects.all()
+        context['categories'] = BlogCategory.objects.annotate(count=Count('blog')).order_by('-count')
         return context
 
 
