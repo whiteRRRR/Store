24a678de9af9c2da2389807bef050090abcbc720
whiteRR <whiterr2134@gmail.com>
2023-07-12 16:10:52 +0600

    Added new URL template tags and category

--- config/apps/blog/urls.py
+++ config/apps/blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', index, name='main_page'),
     path('blog_list/', BlogListView.as_view(), name='blog_list'),
     path('blog_list/search', BlogSearchView.as_view(), name='blog_search'),
+    path('blog_list/category/<slug:category_slug>/', ByCategoryView.as_view(), name='by_category'),
+    path('blog_list/tags/<slug:tag_slug>/', ByTagView.as_view(), name='by_tag'),
     path('blog_list/<slug:slug>/', BlogDetailView.as_view(), name='blog_detail')
 ]
