0d46a392eebfe2b278af2f51706ff1045aa9f6fd
Askar Social <ernur.social@bk.ru>
2023-07-13 12:43:49 +0600

    changed named of url from 'by_category' to 'by_product_category'

--- config/apps/product/urls.py
+++ config/apps/product/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='homepage'),
     path('products/product_list/', ProductListView.as_view(), name='product_list'),
-    path('products/product_list/category/<slug:category_slug>/', ByCategoryView.as_view(), name='by_category'),
+    path('products/product_list/category/<slug:category_slug>/', ByCategoryView.as_view(), name='by_product_category'),
     path('products/product_list/search', ProductSearchView.as_view(), name='search_product'),
     path('products/product_list/<slug:product_name>/', ProductDetailView.as_view(), name='product_detail'),
 ]
