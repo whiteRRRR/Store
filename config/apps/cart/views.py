42ad6410e62fe85a82ff2f0dee6ab19ea27a2e18
Askar Social <ernur.social@bk.ru>
2023-07-15 15:27:31 +0600

    created views for cart

--- config/apps/cart/views.py
+++ config/apps/cart/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Order
+
+
+def cart(request):
+
+    name = 'Корзина'
+
+    if request.user.is_authenticated:
+        user = request.user
+        order, created = Order.objects.get_or_create(user=user.id, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order, 'name': name}
+    return render(request, 'cart/cart.html', context=context)
+
+
+def checkout(request):
+
+    name = 'Заказ'
+
+    if request.user.is_authenticated:
+        user = request.user.user_id
+        order, created = Order.objects.get_or_create(user=user, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order, 'name': name}
+    return render(request, 'cart/checkout.html', context=context)
