13266ba836221d5dd9388cb3006185bd061db4bb
whiteRR <whiterr2134@gmail.com>
2023-07-30 21:30:15 +0600

    Created url addresses for cart

---
+++ config/apps/cart/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('cart_list/', CartView.as_view(), name='cart'),
+    path('add/<slug:product_slug>/', AddToCartView.as_view(), name='add_to_cart'),
+    path('remove/<slug:product_slug>/', RemoveFromCartView.as_view(), name='remove_from_cart'),
+]
