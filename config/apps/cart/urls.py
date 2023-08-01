e6da2dc8c988619546b70133e23a023a500b720a
Askar Social <ernur.social@bk.ru>
2023-08-01 18:11:49 +0600

    added new url

--- config/apps/cart/urls.py
+++ config/apps/cart/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('cart_list/', CartView.as_view(), name='cart'),
     path('add/<slug:product_slug>/', AddToCartView.as_view(), name='add_to_cart'),
     path('remove/<slug:product_slug>/', RemoveFromCartView.as_view(), name='remove_from_cart'),
+    path('checkout', CheckoutView.as_view(), name='checkout')
 ]
