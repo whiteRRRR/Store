4992fd34b274b2d6c3ebf1a5a6bb7f1b9ec6806f
Askar Social <ernur.social@bk.ru>
2023-08-02 16:37:36 +0600

    added cache

--- config/apps/blog/urls.py
+++ config/apps/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from .views import *
 
 urlpatterns = [
     path('', index, name='main_page'),
-    path('blog_list/', BlogListView.as_view(), name='blog_list'),
+    path('blog_list/', cache_page(300)(BlogListView.as_view()), name='blog_list'),
     path('blog_list/search', BlogSearchView.as_view(), name='blog_search'),
     path('blog_list/category/<slug:category_slug>/', ByCategoryView.as_view(), name='by_category'),
     path('blog_list/tags/<slug:tag_slug>/', ByTagView.as_view(), name='by_tag'),
--- config/apps/product/urls.py
+++ config/apps/product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
 urlpatterns = [
     path('', index, name='homepage'),
-    path('products/product_list/', ProductListView.as_view(), name='product_list'),
+    path('products/product_list/', cache_page(300)(ProductListView.as_view()), name='product_list'),
     path('products/product_list/category/<slug:category_slug>/', ByCategoryView.as_view(), name='by_product_category'),
     path('products/product_list/search', ProductSearchView.as_view(), name='search_product'),
     path('products/product_list/<slug:product_name>/', ProductDetailView.as_view(), name='product_detail'),
