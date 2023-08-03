8031b344cfd1a1b8e08d2d7aa956b5955c230226
whiteRR <whiterr2134@gmail.com>
2023-08-03 17:22:38 +0600

    Created new view

--- config/apps/product/views.py
+++ config/apps/product/views.py
@@ -9,11 +9,17 @@ def index(request):
     return render(request, 'base/index.html')
 
 
+class TrendProductListView(ListView):
+    model = Products
+    template_name = 'base/index.html'
+    context_object_name = 'products'
+
+
 class ProductListView(DataMixin, ListView):
     model = Products
     template_name = 'product/product_list.html'
     context_object_name = 'products'
-    paginate_by = 1
+    paginate_by = 6
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,7 +31,7 @@ class ByCategoryView(DataMixin, ListView):
     model = Products
     template_name = 'product/product_list.html'
     context_object_name = 'products'
-    paginate_by = 1
+    paginate_by = 6
 
     def get_queryset(self):
         category_slug = self.kwargs['category_slug']
@@ -58,7 +64,7 @@ class ProductDetailView(DetailView):
 class ProductSearchView(DataMixin, ListView):
     template_name = 'product/product_list.html'
     context_object_name = 'products'
-    paginate_by = 1
+    paginate_by = 6
 
     def get_queryset(self):
         return Products.objects.filter(name__icontains=self.request.GET.get("search"))
