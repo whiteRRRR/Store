02cd31ba086efc97920903c570958d2b459e46af
whiteRR <whiterr2134@gmail.com>
2023-08-03 17:22:09 +0600

    Update urls

--- config/apps/product/urls.py
+++ config/apps/product/urls.py
@@ -4,7 +4,7 @@ from django.views.decorators.cache import cache_page
 from .views import *
 
 urlpatterns = [
-    path('', index, name='homepage'),
+    path('', TrendProductListView.as_view(), name='homepage'),
     path('products/product_list/', cache_page(300)(ProductListView.as_view()), name='product_list'),
     path('products/product_list/category/<slug:category_slug>/', ByCategoryView.as_view(), name='by_product_category'),
     path('products/product_list/search', ProductSearchView.as_view(), name='search_product'),
