61e89a893fea78bc91adb63535c05feb0495b246
whiteRR <whiterr2134@gmail.com>
2023-08-03 18:29:52 +0600

    merged CheckoutView with OrderView

--- config/apps/cart/views.py
+++ config/apps/cart/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import FormView
 
 from .models import CartItem, OrderItem
 from .forms import OrderForm
-from .utils import DataMixin
+from ..product.models import Products
 
 
 class AddToCartView(View):
@@ -38,22 +39,18 @@ class CartView(View):
         return render(request, 'cart/cart.html', {'cart_items': cart_items, 'total': total})
 
 
-class CheckoutView(View):
-    def get(self, request):
-        cart_items = CartItem.objects.filter(user=request.user)
-        total = sum(item.total_price() for item in cart_items)
-        return render(request, 'cart/checkout.html', {'cart_items': cart_items, 'total': total})
-
-
-class OrderView(DataMixin, FormView):
+class OrderView(FormView):
     form_class = OrderForm
     template_name = 'cart/checkout.html'
-    success_url = 'base/index.html'
+    success_url = reverse_lazy('homepage')
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(name='checkout')
-        return dict(list(context.items()) + list(c_def.items()))
+        cart_items = CartItem.objects.filter(user=self.request.user)
+        total = sum(item.total_price() for item in cart_items)
+        context['cart_items'] = cart_items
+        context['total'] = total
+        return context
 
     def form_valid(self, form):
         first_name = form.cleaned_data['first_name']
@@ -64,6 +61,7 @@ class OrderView(DataMixin, FormView):
         city = form.cleaned_data['city']
         zipcode = form.cleaned_data['zip_code']
         order = OrderItem.objects.create(
+            user=self.request.user,
             first_name=first_name,
             last_name=last_name,
             phone_number=phone_number,
@@ -72,8 +70,8 @@ class OrderView(DataMixin, FormView):
             city=city,
             zipcode=zipcode
         )
-
         return super().form_valid(form)
+
 
 
 
