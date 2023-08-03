8c917333363aac75c4186e10f8c36ec9eb683131
Askar Social <ernur.social@bk.ru>
2023-08-03 12:23:39 +0600

    added autofill to slug

--- config/apps/product/admin.py
+++ config/apps/product/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+
 class ProductImageAdmin(admin.StackedInline):
     model = ProductImages
+
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'is_published')
+    prepopulated_fields = {'slug': ('name',)}
     inlines = [ProductImageAdmin]
 
     class Meta:
         model = Products
+
 
 admin.site.register(ProductCategory)
