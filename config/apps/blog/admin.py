9474217dc03d54054d1f014ccdb0abe0a53cfd49
whiteRR <whiterr2134@gmail.com>
2023-08-04 17:11:51 +0600

    Added list_display field

--- config/apps/blog/admin.py
+++ config/apps/blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Blog)
-admin.site.register(BlogCategory)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+
+    def display_tags(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+
+    display_tags.short_description = 'Тэги'
+    list_display = ('title', 'category', 'display_tags', 'date')
+    prepopulated_fields = {"slug": ('title',)}
+
+
+admin.register(BlogCategory)
 admin.site.register(BlogTags)
