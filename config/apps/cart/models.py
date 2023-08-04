9013e1622f8b3267c85222156daf00abf4abf5ed
whiteRR <whiterr2134@gmail.com>
2023-08-04 17:13:05 +0600

    Updated slug field to unique and added class Meta

--- config/apps/cart/models.py
+++ config/apps/cart/models.py
@@ -5,6 +5,7 @@ from apps.user.models import CustomUser
 
 
 class CartItem(models.Model):
+    """Модель для корзины"""
     product = models.ForeignKey('product.Products', on_delete=models.CASCADE)
     user = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
@@ -12,8 +13,13 @@ class CartItem(models.Model):
     def total_price(self):
         return self.quantity * self.product.price
 
+    class Meta:
+        verbose_name = 'Корзина товара пользователя'
+        verbose_name_plural = 'Корзина товаров пользователя'
+
 
 class OrderItem(models.Model):
+    """Модель для оформления заказа"""
     user = models.ForeignKey('user.CustomUser', on_delete=models.PROTECT)
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
@@ -23,3 +29,7 @@ class OrderItem(models.Model):
     country = models.CharField(max_length=100, null=True)
     city = models.CharField(max_length=100, null=True)
     zipcode = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        verbose_name = 'Оформленный заказ'
+        verbose_name_plural = 'Оформленные заказы'
