224534f7a8f21af0e20ea005b1fbc8a02d53ca84
whiteRR <whiterr2134@gmail.com>
2023-08-04 17:14:12 +0600

    Added admin panel for CartItem and OrderItem class

--- config/apps/cart/admin.py
+++ config/apps/cart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CartItem, OrderItem
 
-# Register your models here.
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'quantity', 'product', 'total_price')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'country', 'address')
